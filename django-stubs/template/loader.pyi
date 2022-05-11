9e591936773df8b7484a4294bfff276d1d87ec1c
Collin Anderson <cmawebsite@gmail.com>
2022-05-11 12:59:44 -0400

    get_template and select_template return Template

--- django-stubs/template/loader.pyi
+++ django-stubs/template/loader.pyi
@@ -1,16 +1,17 @@
 from typing import Any, Dict, List, Optional, Union
 
 from django.http.request import HttpRequest
+from django.template.base import Template
 from django.template.exceptions import (  # noqa: F401
     TemplateDoesNotExist as TemplateDoesNotExist,
 )
 
 from . import engines as engines  # noqa: F401
 
-def get_template(template_name: str, using: Optional[str] = ...) -> Any: ...
+def get_template(template_name: str, using: Optional[str] = ...) -> Template: ...
 def select_template(
     template_name_list: Union[List[str], str], using: Optional[str] = ...
-) -> Any: ...
+) -> Template: ...
 def render_to_string(
     template_name: Union[List[str], str],
     context: Optional[Dict[str, Any]] = ...,
